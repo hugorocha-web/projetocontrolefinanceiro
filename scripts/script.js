81bfdd969b2a0263efe1befde352d29550e2d2da
hugorocha-web <hjj78092@gmail.com>
2026-09-08 18:16:41 -0300

    mais alguns ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,14 @@ let btnoperacao = document.querySelector('#btn')
 let popup = document.querySelector('.fundo-escuro')
 let operacoes = []
 let valorsaldo = 0;
-
+let valorentradas = 0;
+let valorsaida = 0;
+let alimentação = 0
+let transporte = 0
+let lazer = 0
+let appali = document.querySelector('#valorali')
+let apptrans = document.querySelector('#valortrans')
+let applazer = document.querySelector('#valorlazer')
 
 
 
@@ -29,11 +36,26 @@ function addnovaop(){
         if (!isNaN(valor)) {
             if (tipo ==='entrada') {
                 valorsaldo+=valor
-            } else {
+                valorentradas+=valor
+                if (categoria === 'alimentação') {
+                    alimentação+=valor
+                    console.log('aq')
+                }
+                else if(categoria  ==='transporte'){
+                    transporte+=valor
+                }
+                else{
+                    lazer+=valor
+                }
+            } 
+            else {
                 valorsaldo-=valor
+                valorsaida+=valor
             }
             console.log(valorsaldo)
-            atualizardados(valorsaldo)
+            atualizardadosgrafico(alimentação, transporte, lazer)
+            atualizardados(valorsaldo, valorentradas, valorsaida)
+            
             popup.classList.remove('popup')
             document.querySelector('#labelvalor').value = ''
         } else {
@@ -49,6 +71,13 @@ function addnovaop(){
 
     
 }
-function atualizardados(saldovalor, entradas, despesas){
+function atualizardados(saldovalor, entradasvalor, despesas){
     saldo.textContent = 'R$ ' + saldovalor
+    entradas.textContent = 'R$ ' + entradasvalor
+    saida.textContent = 'R$ ' + despesas
+}
+function atualizardadosgrafico(alimentaçãoapp, trasn, laz){
+    appali.textContent = 'R$ ' + alimentaçãoapp
+    apptrans.textContent = 'R$ ' + trasn
+    applazer.textContent = 'R$ ' + laz
 }
