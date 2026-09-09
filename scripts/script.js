3ff52d96374bd1b008e405ab19627536e2310609
hugorocha-web <hjj78092@gmail.com>
2026-09-09 13:22:04 -0300

    adicionando mais coisas

--- scripts/script.js
+++ scripts/script.js
@@ -13,63 +13,77 @@ let lazer = 0
 let appali = document.querySelector('#valorali')
 let apptrans = document.querySelector('#valortrans')
 let applazer = document.querySelector('#valorlazer')
+let clonado = document.querySelector('#entradabtn')
+let clonadosaida = document.querySelector('#saidabtn')
 
 
 
 btnoperacao.addEventListener('click', addnovaop)
-
+let btnadicionar = document.querySelector('#add')
+btnadicionar.addEventListener('click', adicionar)
 function addnovaop(){
     popup.classList.toggle('popup')
-    let btnadicionar = document.querySelector('#add')
-    btnadicionar.addEventListener('click', ()=>{
-        let descricao = document.querySelector('#labeldescrição').value
+}
+let btncancelar = document.querySelector('#can')
+btncancelar.addEventListener('click', ()=>{
+    popup.classList.remove('popup')
+})
+function adicionar(){
+    let descricao = document.querySelector('#labeldescrição').value
         
-        let tipo = document.querySelector('#labeltipo').value
+    let tipo = document.querySelector('#labeltipo').value
         
-        let categoria = document.querySelector('#labelcate').value
+    let categoria = document.querySelector('#labelcate').value
         
-        let valor = document.querySelector('#labelvalor').value
-        
-        valor =Number(valor)
-
-        
-        if (!isNaN(valor)) {
-            if (tipo ==='entrada') {
-                valorsaldo+=valor
-                valorentradas+=valor
-                if (categoria === 'alimentação') {
-                    alimentação+=valor
-                    console.log('aq')
-                }
-                else if(categoria  ==='transporte'){
-                    transporte+=valor
-                }
-                else{
-                    lazer+=valor
-                }
-            } 
-            else {
-                valorsaldo-=valor
-                valorsaida+=valor
-            }
-            console.log(valorsaldo)
-            atualizardadosgrafico(alimentação, transporte, lazer)
-            atualizardados(valorsaldo, valorentradas, valorsaida)
-            
-            popup.classList.remove('popup')
-            document.querySelector('#labelvalor').value = ''
-        } else {
-            return
-        }
-        console.log(popup)
-
-    })
-    let btncancelar = document.querySelector('#can')
-    btncancelar.addEventListener('click', ()=>{
-        popup.classList.remove('popup')
-    })
-
+    let valor = document.querySelector('#labelvalor').value
     
+        
+        
+    if(valor ===''){
+        return
+    }
+    valor = Number(valor)
+    if (!isNaN(valor)) {
+        operacoes.unshift({
+        'descricao':descricao,
+        'tipo':tipo,
+        'categoria':categoria,
+        'valor':valor
+        })
+        if (tipo ==='entrada') {
+            valorsaldo+=valor
+            valorentradas+=valor
+                
+        } 
+        else {
+            if (categoria === 'alimentação') {
+                alimentação+=valor
+                console.log('aq')
+            }
+            else if(categoria  ==='transporte'){
+                transporte+=valor
+            }
+            else{
+                lazer+=valor
+            }
+            valorsaldo-=valor
+            valorsaida+=valor
+        }
+        console.log(valorsaldo)
+        atualizardadosgrafico(alimentação, transporte, lazer)
+        atualizardados(valorsaldo, valorentradas, valorsaida)
+        atualizarpopus()
+            
+        popup.classList.remove('popup')
+        document.querySelector('#labelvalor').value = ''
+    
+    }
+     else {
+        return
+    }
+    console.log(popup)
+    console.log(operacoes)
+
 }
 function atualizardados(saldovalor, entradasvalor, despesas){
     saldo.textContent = 'R$ ' + saldovalor
@@ -80,4 +94,51 @@ function atualizardadosgrafico(alimentaçãoapp, trasn, laz){
     appali.textContent = 'R$ ' + alimentaçãoapp
     apptrans.textContent = 'R$ ' + trasn
     applazer.textContent = 'R$ ' + laz
+    let todos = [alimentaçãoapp, trasn, laz]
+    let maior = 0;
+    for (let i = 0; i<todos.length; i++){
+        
+        if(todos[i] > maior){
+            maior = todos[i]
+        }
+        
+    }
+    let graficoali = (alimentaçãoapp/ maior) * 100
+    let graficotrans = (trasn/ maior) * 100
+    let graficolaz = (laz/ maior) * 100
+    let divali = document.querySelector('#tamanhoalimentação')
+    let divtrans = document.querySelector('#tamanhotransporte')
+    let divlaz = document.querySelector('#tamanholazer')
+    divali.style.width = graficoali + '%'
+    divtrans.style.width = graficotrans + '%'
+    divlaz.style.width = graficolaz + '%'
+    
 }
+function atualizarpopus(){
+    let secao = document.querySelector('#secao4')
+    secao.innerHTML = ''
+    
+    for(let i =0 ; operacoes.length > i ; i++){
+        
+        if(operacoes[i].tipo ==='entrada'){
+            let clone = clonado.cloneNode(true)
+            clone.querySelector('#descrição').textContent = operacoes[i].descricao
+            clone.querySelector('#valorentrada').textContent = 'R$ ' + operacoes[i].valor
+            clone.style.display = 'flex'
+            secao.appendChild(clone)
+            
+
+        }
+        else{
+            let clone = clonadosaida.cloneNode(true)
+            clone.querySelector('#descrição').textContent = operacoes[i].descricao
+            clone.querySelector('#valorsaida').textContent = 'R$ ' + operacoes[i].valor
+            clone.style.display = 'flex'
+            secao.appendChild(clone)
+
+
+        }
+
+    }
+}
+window.onload
